e3c8a2144ab32d5e8d0e57f9b594793a54c0fd87
m-526 <25r1116.hirata@gmail.com>
2026-02-13 14:52:03 +0900

    変更

--- seed.py
+++ seed.py
@@ -7,6 +7,10 @@ app = create_app()
 with app.app_context():
     db = get_db()
     
+    # sqlファイルを読み込んで実行
+    with app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+        
     # 1. ユーザーデータの初期化
     db.execute('DELETE FROM user')
     db.execute('DELETE FROM sqlite_sequence WHERE name="user"')
